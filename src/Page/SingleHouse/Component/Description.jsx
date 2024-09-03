f6628e5f6034a4f810989574c58d4cf94e7c6dfc
MD AL AMIN ISLAM <mdalamin212104@gmail.com>
2024-09-03 22:01:22 +0600

    single page design added

--- src/Page/SingleHouse/Component/Description.jsx
+++ src/Page/SingleHouse/Component/Description.jsx
@@ -1,4 +1,7 @@
 import { CiLocationOn } from "react-icons/ci";
+import { FaBaby } from "react-icons/fa";
+import { GiBed, GiFurnace } from "react-icons/gi";
+import { LiaBedSolid } from "react-icons/lia";
 import { useLoaderData } from "react-router-dom";
 
 const Description = () => {
@@ -21,7 +24,7 @@ const Description = () => {
     Status,
   } = data;
   return (
-    <div>
+    <div className="md:flex gap-4">
       <div className="md:w-3/4">
         <div className="flex text-xl font-bold text-black">
           <h1>{title}</h1>
@@ -45,34 +48,145 @@ const Description = () => {
         <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-4 my-4">
           <div className="card">
             <figure>
-              <img src="/image/interior1.jpeg" alt="Shoes" className="w-full h-[160px]" />
+              <img
+                src="/image/interior1.jpeg"
+                alt="Shoes"
+                className="w-full h-[160px]"
+              />
             </figure>
           </div>
           <div className="card">
             <figure>
-              <img src="/image/interior2.jpeg" alt="Shoes" className="w-full h-[160px]" />
+              <img
+                src="/image/interior2.jpeg"
+                alt="Shoes"
+                className="w-full h-[160px]"
+              />
             </figure>
           </div>
           <div className="card">
             <figure>
-              <img src="/image/interior3.jpeg" alt="Shoes" className="w-full h-[160px]" />
+              <img
+                src="/image/interior3.jpeg"
+                alt="Shoes"
+                className="w-full h-[160px]"
+              />
             </figure>
           </div>
         </div>
-        <p>Min Price: {min_price}</p>
-        <p>Max Price: {max_price}</p>
-
-        <p>Bedrooms: {bedRoom}</p>
-        <p>Bathrooms: {bathRoom}</p>
-        <p>Balcony: {balcony}</p>
-        <p>Area: {area}</p>
-        <p>Lift: {lift ? "Yes" : "No"}</p>
-        <p>Facing: {facing}</p>
-        <p>Additional Rooms: {additionalRooms}</p>
-        <p>Age of Construction: {AgeOfConstruction}</p>
-        <p>Status: {Status}</p>
+        <div className="card bg-[#E5E7EB] px-6">
+          <h1 className="Montserrat text-2xl font-semibold p-4">Overview</h1>
+          <div className="font-bold flex gap-6 justify-evenly m-2 bg-white rounded p-2">
+            <h1 className="flex text-[16px] gap-2">
+              <LiaBedSolid className="mt-1" />
+              {bedRoom} Beds
+            </h1>
+            <h1 className="flex text-[16px] gap-2">
+              <GiBed className="mt-1" />
+              {bedRoom} bad
+            </h1>
+            <h1 className="flex text-[16px] gap-2">
+              <FaBaby className="mt-1" />
+              {balcony} Balcony
+            </h1>
+            <h1 className="flex text-[16px] gap-2">
+              <GiFurnace className="mt-1" /> Fully Furnished
+            </h1>
+          </div>
+          <div className="my-4 flex justify-between">
+            <div>
+              <h1 className="text-[#5C5C5C]">Carpet Area</h1>
+              <p>{area}</p>
+            </div>
+            <div>
+              <h1 className="text-[#5C5C5C]">Floor</h1>
+              <p>Second (Out of 6th floor)</p>
+            </div>
+            <div>
+              <h1 className="text-[#5C5C5C]">Transaction Type</h1>
+              <p>Ready to move</p>
+            </div>
+            <div>
+              <h1 className="text-[#5C5C5C]">Lift</h1>
+              <p>{lift ? "Yes" : "No"}</p>
+            </div>
+          </div>
+          <div className="flex justify-between mb-8">
+            <div>
+              <h1 className="text-[#5C5C5C]">Facing</h1>
+              <p>{facing}</p>
+            </div>
+            <div>
+              <h1 className="text-[#5C5C5C]">Additional Rooms</h1>
+              <p>
+                {additionalRooms} servant room & {additionalRooms} gust room
+              </p>
+            </div>
+            <div>
+              <h1 className="text-[#5C5C5C]">Age of construction</h1>
+              <p>{AgeOfConstruction}</p>
+            </div>
+            <div>
+              <h1 className="text-[#5C5C5C]">Status</h1>
+              <p>{Status}</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="md:w-1/4"></div>
+      <div className="md:w-1/4 mt-24">
+        <div className="card bg-[#ECF5FF] Poppins p-4 rounded-none">
+          <h1 className="text-[#6B7280]">property value</h1>
+          <p className="text-2xl font-bold">
+            ${min_price} - ${max_price}
+          </p>
+          <p className="font-medium pt-6">
+            Your bid can not be than 10% of the property Minimum value.
+          </p>
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Min</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </label>
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Max</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </label>
+          <input
+            type="range"
+            min={0}
+            max="100"
+            defaultValue={""}
+            className="range mt-4"
+            step="25"
+          />
+          <div className="flex w-full justify-between px-2 text-xs">
+            <span>${min_price}</span>
+            <span>${max_price}</span>
+          </div>
+          <button className="bg-[#0059B1] text-white font-medium text-[18px] mx-auto btn w-[170px]">
+            Bid Property
+          </button>
+        </div>
+        <div className="card  image-full mt-8 h-[420px] shadow-xl">
+          <figure>
+            <img
+              src="/image/map.jpg"
+              alt="Shoes"
+            />
+          </figure>
+        </div>
+      </div>
     </div>
   );
 };
